b7c58d073e9e8e00e4956c79146a3a1aa5898059
Antonia <antonia.birlog@gmail.com>
2019-12-21 23:15:22 +0200

    make it better

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/form-app/'));  
-app.get('*', (req, res) => {  
+app.get('*', (req, res) => { 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With'); 
   res.status(200).sendFile(__dirname + '/dist/form-app/index.html');  
 });  
 app.get('/hello', (req, res) => res.send('Hello World!'))
