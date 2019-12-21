4604ea8e8ac9443f07fcb03760a2a93b121bf7e8
Antonia <antonia.birlog@gmail.com>
2019-12-21 23:19:26 +0200

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.get('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With'); 
+  res.setHeader( 'Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type');
   res.status(200).sendFile(__dirname + '/dist/form-app/index.html');  
+  
 });  
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
