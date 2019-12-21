744bafe18001cbddc474c7846e69080bc71d2070
Antonia <antonia.birlog@gmail.com>
2019-12-21 22:12:58 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,21 @@
+var PORT = process.env.PORT || 8080
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/dist'));  
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/dist/index.html');  
+});  
+app.get('/hello', (req, res) => res.send('Hello World!'))
+
+app.post('/api/userCreate', (req, res) => {
+	console.log('done')
+    res.json({
+        data: req.body
+    })
+	
+})
+
+
+app.listen(PORT)
