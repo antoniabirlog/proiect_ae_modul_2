e33ebd502c2df51500c0a323c43087eaf6958b9d
Antonia <antonia.birlog@gmail.com>
2019-12-22 11:45:04 +0200

    make it better

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -9,6 +9,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RootComponent implements OnInit {
   userForm;
+  
+  error_messages = {
+    'firstName': [
+      { type: 'required', message: 'First Name is required.' },
+    ],
+
+    'lastName': [
+      { type: 'required', message: 'Last Name is required.' }
+    ],
+
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'minlength', message: 'Email length.' },
+      { type: 'maxlength', message: 'Email length.' },
+      { type: 'required', message: 'please enter a valid email address.' }
+    ],
+
+    'password': [
+      { type: 'required', message: 'password is required.' },
+      { type: 'minlength', message: 'password length.' },
+      { type: 'maxlength', message: 'password length.' }
+    ]
+  }
+  
    constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
